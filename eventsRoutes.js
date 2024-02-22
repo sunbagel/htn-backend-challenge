0cde86fac752b21e6f5b5e11a734ce4ae83dcf58
sunbagel <43621165+sunbagel@users.noreply.github.com>
2024-02-22 14:44:58 -0500

    create formatting for event_registrations route

--- eventsRoutes.js
+++ eventsRoutes.js
@@ -43,8 +43,72 @@ router.post("/events", async (req, res) => {
     
 });
 
-router.get("/api/event_registrations", async (req, res) => {
+router.get("/event_registrations", async (req, res) => {
 
+    const { userID, eventID, startTime, endTime } = req.query;
+    
+    let selectQueries = [];
+    let eventJoins = [];
+    let userJoins = [];
+    let whereQueries = [];
+    let queryValues = [];
+
+    if(startTime){
+        eventJoins.push("e.start_time = ?");
+        queryValues.push(startTime);	
+    }
+
+    if(endTime){
+        eventJoins.push("e.end_time = ?");
+        queryValues.push(endTime);
+    }
+
+    if(eventID){
+        selectQueries.push("e.*");
+        whereQueries.push("er.event_id = ?");
+        eventJoins.push("e.id = er.event_id");
+        queryValues.push(eventID);
+    }
+
+    if(eventID == null && userID == null){
+        selectQueries.push("*");
+    }
+
+    if(userID){
+    //	maybe only want u.id, u.name, u.email
+        selectQueries.push("u.*");
+        whereQueries.push("er.user_id = ?");
+        userJoins.push("u.id = er.user_id");
+        queryValues.push(userID);
+    }
+
+
+    const selectClause = selectQueries.join(",");
+    const whereClause = whereQueries.join(" AND ");
+
+    let eventClause = "";
+    let userClause = "";
+    if(eventJoins.length > 0){
+        
+        eventClause = "JOIN events e ON " + eventJoins.join(" AND ");
+    }
+
+    if(userJoins.length > 0){
+        userClause = "JOIN users u ON " + userJoins.join(" AND ");
+    }
+
+    const query = `SELECT ${selectClause}
+            FROM event_registrations er
+            ${eventClause}
+            ${userClause}
+            WHERE ${whereClause}`;
+
+    console.log(query);
+    console.log(queryValues);
+
+    // const eventRegistrations = db.all(query, queryValues);
+
+    res.status(200).json();
 
 
 });
